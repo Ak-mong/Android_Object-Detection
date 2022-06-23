ef03a6a27f265a96307b8ed2b55fa3e7352169f7
kyr2880 <33219651+kyr2880@users.noreply.github.com>
2022-06-23 14:20:14 +0900

    인증화면 추가

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/CertificationActivity.java
@@ -0,0 +1,28 @@
+package org.tensorflow.lite.examples.detection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CertificationActivity extends AppCompatActivity {
+
+    private Button btn_move;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certification);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CertificationActivity.this, DetectorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
